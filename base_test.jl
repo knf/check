1f0a3b2f62333382e247bf3baa32d857b8f3e72c
Nicholas <nick@targetmobi.com>
2012-04-27 11:55:45 -0500

    lastest changes

--- base_test.jl
+++ base_test.jl
@@ -5,30 +5,29 @@ b = "bar"
 email_regex = "^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})"
 
 function test_first()
-	check_equal(1, 1, "Numbers are not equal")
-	check_not_equal(1, 2, "Numbers are equal")
-	check_type_of("string", String, "Types don't match")
-	check_same(f, :foo, "Object is not same")
-	check_not_same(f, b, "Object is same and should not be")
-	check_match("email@host.com", email_regex, "No Match")
-	check_no_match("spamhost.com", email_regex, "Matches and should not match")
-	check_string("string", "Not a String")
-	check_int(123, "Not an Integer")
+  check_equal(1, 1, "Numbers are not equal")
+  check_not_equal(1, 2, "Numbers are equal")
+  check_type_of("string", String, "Types don't match")
+  check_same(f, :foo, "Object is not same")
+  check_not_same(f, b, "Object is same and should not be")
+  check_match("email@host.com", email_regex, "No Match")
+  check_no_match("spamhost.com", email_regex, "Matches and should not match")
+  check_string("string", "Not a String")
+  check_int(123, "Not an Integer")
 end
 
 function test_second()
-	check_equal(1, 2, "Numbers are not equal")
-	check_not_equal(2, 2, "Numbers are equal")
-	check_type_of(2, String, "Types don't match")
-	check_same(f, :food, "Object is not same")
-	check_not_same(f, f, "Object is same and should not be")
-	check_match("emailhost.com", email_regex, "No Match")
-	check_no_match("spamhost@dd.com", email_regex, "Matches and should not match")
-	check_string(2, "Not a String")
-	check_int("string", "Not an Integer")
+  check_equal(1, 2, "Numbers are not equal")
+  check_not_equal(2, 2, "Numbers are equal")
+  check_type_of(2, String, "Types don't match")
+  check_same(f, :food, "Object is not same")
+  check_not_same(f, f, "Object is same and should not be")
+  check_match("emailhost.com", email_regex, "No Match")
+  check_no_match("spamhost@dd.com", email_regex, "Matches and should not match")
+  check_string(2, "Not a String")
+  check_int("string", "Not an Integer")
 end
 
 test_first()
 test_second()
 
-   
--- lib.jl
+++ lib.jl
@@ -1,58 +1,58 @@
 function check(func, msg...)
-	try
-		assert(func)
-		println("Success.....")
-	catch e
-		println("Failure: $msg")
-	end
+  try
+    assert(func)
+    println("Success.....")
+  catch e
+    println("Failure: $msg")
+  end
 end
 
 function check_equal(arg1, arg2, msg...)
-	check(isequal(arg1, arg2), msg)
+  check(isequal(arg1, arg2), msg)
 end
 
 function check_not_equal(arg1, arg2, msg...)
-	check(!isequal(arg1, arg2), msg)
+  check(!isequal(arg1, arg2), msg)
 end
 
 function check_type_of(obj, obj_type, msg...)
-	check(isa(obj,obj_type), msg)
+  check(isa(obj,obj_type), msg)
 end
 
 function check_same(arg1, arg2, msg...)
-	check(is(arg1, arg2), msg)
+  check(is(arg1, arg2), msg)
 end
 
 function check_not_same(arg1, arg2, msg...)
-	check(!is(arg1, arg2), msg)
+  check(!is(arg1, arg2), msg)
 end
 
 function check_match(test_string, regex, msg...)
-	if matches(r"$regex", test_string)
-		println("Success.....")
-		return true
-	else
-		println("Failure: $msg")
-		return false
-	end
+  if matches(r"$regex", test_string)
+    println("Success.....")
+    return true
+  else
+    println("Failure: $msg")
+    return false
+  end
 end
 
 function check_no_match(test_string, regex, msg...)
-	if !matches(r"$regex", test_string)
-		println("Success.....")
-		return true
-	else
-		println("Failure: $msg")
-		return false
-	end
+  if !matches(r"$regex", test_string)
+    println("Success.....")
+    return true
+  else
+    println("Failure: $msg")
+    return false
+  end
 end
 
 function check_string(string, msg...)
-	check(isa(string,String), msg)
+  check(isa(string,String), msg)
 end
 
 function check_int(int, msg...)
-	check(isa(int,Int), msg)
+  check(isa(int,Int), msg)
 end
 
 
@@ -64,4 +64,3 @@ end
 
 
 
-   
